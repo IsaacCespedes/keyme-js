bad2cb21500b748d143c17dd940d311db0df55db
isaaccespedes <isaac.cespedes1@gmail.com>
2020-02-23 03:44:44 +0000

    Switching to pug markup. Starting on the ProductListPage. Keeping copies of the Vuetify Boilerplate as temporary reference.

---
+++ isaac-cespedes-ui/src/components/ProductListPage.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  v-container
+    v-row
+      v-col(class="pb-1 ml-4 deep-orange--text" cols="12")
+        h2 Add or Scan Your Free Key
+    v-row
+      v-col(class="ml-4 mr-2 light-blue--text" cols="12")
+        h3(class="pb-0 blueUnderline") FREE DELIVERY
+</template>
+
+<script>
+export default {
+  name: "ProductListPage",
+
+  data: () => ({})
+};
+</script>
+
+<style lang="scss">
+.blueUnderline {
+  border-bottom: 3px solid #03a9f4;
+}
+</style>
